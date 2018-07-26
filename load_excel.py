76ce0f35d5043090c05a740a296d5026a3b14772
alaragebes <alaragb@gmail.com>
2018-07-26 14:33:15 -0700

    added new cross-ref

--- load_excel.py
+++ load_excel.py
@@ -1,18 +1,27 @@
 import openpyxl
 import sys
 
-wb = openpyxl.load_workbook('/Users/alaragebes/Desktop/solar_data/STATES/Louisiana.xlsx')
+##load file
+wb = openpyxl.load_workbook('/Users/alaragebes/Desktop/solar_data/STATES/Wisconsin.xlsx')
 change = wb['Cross-Ref']
+sheet = wb['Sheet1']
+ru = sheet.max_row
 r = change.max_row
 
+def change_name(input):
+    new_value = input.replace(' ','')
+    short_value = new_value[:12]
+    return short_value
+
+##add customer and account in list
 common = []
 for i in range(2,r+1):
-        common.append(change.cell(row=i, column=3).value)
+    common.append(change.cell(row=i, column=3).value)
 
 for i in range(2,r+1):
-        common.append(change.cell(row=i, column=5).value)
+    common.append(change.cell(row=i, column=5).value)
 
-
+##remove duplicates
 result = set()
 for name in common:
     if name != None :
@@ -20,3 +29,30 @@ for name in common:
 
 print result
 print len(result)
+
+## get names
+yelp = set()
+for i in range(2, r+1):
+    if change.cell(row=i, column=1).value != None:
+        yelp.add(change.cell(row=i, column=1).value)
+
+print len(yelp)
+
+##remove the duplicates
+sub = yelp - result
+call_list = list(sub)
+print "the number of new companies: %r" %(len(call_list))
+change.cell(row=1, column=8).value = 'Call List'
+change.cell(row=1, column=9).value = 'URL'
+change.cell(row=1, column=10).value = 'Phone'
+wb.save('/Users/alaragebes/Desktop/solar_data/STATES/Wisconsin.xlsx')
+
+
+
+for i in range(2, (len(call_list)+2)):
+    change.cell(row=i, column=8).value = call_list[i-2]
+    for num in range(2, ru+1):
+        if change_name(sheet.cell(row=num, column=1).value) == call_list[i-2]:
+            change.cell(row=i, column=9).value = sheet.cell(row=num, column=6).value
+            change.cell(row=i, column=10).value = sheet.cell(row=num, column=7).value
+            wb.save('/Users/alaragebes/Desktop/solar_data/STATES/Wisconsin.xlsx')
